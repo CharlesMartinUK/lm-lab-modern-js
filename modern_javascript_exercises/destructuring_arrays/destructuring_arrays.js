f77cad51ca13f446f334ebeaccaa1d0f2cc483fa
CharlesMartinUK <charles_martin@hotmail.co.uk>
2023-07-16 18:45:39 +0100

    Did destructuring arrays task

--- modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
+++ modern_javascript_exercises/destructuring_arrays/destructuring_arrays.js
@@ -1,9 +1,13 @@
 // Instructions can be found in destructuring_arrays.md
 
 export function sequence(order) {
+  /*
   const first = order[0];
   const second = order[1];
   const third = order[2];
+	*/
+  const [first, second, third] = order;
 
+  //console.log(first)
   return first + ", " + second + ", " + third;
 }
